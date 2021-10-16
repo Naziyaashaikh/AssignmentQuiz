a0b0d51cde4df82d6f02641b33fb3ab84a3735e7
Naziyaashaikh <92212084+Naziyaashaikh@users.noreply.github.com>
2021-10-16 23:41:58 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,29 @@
+const question=[
+    {
+        question:"Bar soap and water are fine for cleansing the face if you have sensitive or dry skin.",
+        answer:"True",
+        option:["True","False","None","All"]
+    },
+    {
+        question:"Identify the proper order for facial cleansing.",
+        answer:"Cleanse,rinse and moisturize",
+        option:["Cleanse,rinse and moisturize","rinse,Cleanse and moisturize","moisturize,Cleanse and rinse","moisturize, rinse and Cleanse "]    
+    },
+    {
+        question:"In terms of beauty, the forehead, nose, and chin are referred to as the...",
+        answer:"Tzone",
+        option:["Dryzone","Tzone","Oilyzone","Controlzone"]
+    },
+    {
+        question:"Mascara should be replaced every ___________ months.",
+        answer:"3",
+        option:["3","6","8","18"]
+    },
+    {
+        question:"The term pigment refers to _________________",
+        answer:"Color",
+        option:["Powder","Color","Tanning","Having pig-like skin"]
+    }
+];
+
+module.exports=question;
